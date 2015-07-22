64ea4ea55b7ae7db56a0fc3e140fbfab4bcbb427
Denys Butenko <ua13dark@gmail.com>
2015-07-22 19:08:36 +0300

    Remove gulp-minify-html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -50,7 +50,6 @@ gulp.task('html', ['styles'], () => {
     .pipe($.if('*.css', $.minifyCss({compatibility: '*'})))
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
     .pipe(gulp.dest('dist'));
 });
 
