1634c271e9f7e8822aaa795cabad4131575c679b
Denys Butenko <ua13dark@gmail.com>
2015-07-27 17:30:19 +0300

    Apply JS Beautify for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,27 @@ module.exports = function(type) {
       path: path.join(__dirname, 'dist', 'js'),
       filename: 'main.js'
     },
-    debug : isDev,
+    debug: isDev,
     module: {
-      loaders: [
-        { test: /\.scss$/, loader: "style!css!sass" },
-
-        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
-        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&minetype=application/font-woff" },
-        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
-      ]
+      loaders: [{
+        test: /\.scss$/,
+        loader: "style!css!sass"
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&minetype=application/font-woff"
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&minetype=application/font-woff"
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&minetype=application/octet-stream"
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&minetype=image/svg+xml"
+      }]
     },
     plugins: [
       new webpack.ProvidePlugin({
@@ -39,7 +49,7 @@ module.exports = function(type) {
     ]
   };
 
-  if(isDev){
+  if (isDev) {
     config.devtool = 'eval';
   }
 
