37f5f6f16a34c11086e2046d7f502c119d277f1d
Denys Butenko <ua13dark@gmail.com>
2015-07-31 19:47:02 +0300

    Update Navbar

--- app/scripts/Navbar.jsx
+++ app/scripts/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from "react/addons";
 
-var Navbar = React.createClass({
-  render: function() {
-    return (<nav className="navbar navbar-fixed-top navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="#">LOGO</a>
+export default
+class Navbar extends React.Component {
+    render() {
+      return (<nav className="navbar navbar-fixed-top navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Modern <img src="apple-touch-icon.png" alt="Modern Frontend" />rontend</a>
+          </div>
         </div>
-      </div>
-    </nav>)
+      </nav>)
+    }
   }
-});
-export default Navbar;
